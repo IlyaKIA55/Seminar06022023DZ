62e77807d7edc8eb5421b2cc6c1ad74bb51375f8
Ilya <jyg3a125@gmail.com>
2023-02-08 21:37:58 +0500

    add Z3

---
+++ Z3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.WriteLine("Введите 8 чисел через запятую");
+string [] strArray = Console.ReadLine().Split(',');
+
+int[] result = new int[strArray.Length];
+
+for (int i = 0; i < result.Length; i++)
+{
+    result[i] = int.Parse(strArray[i]);
+}
+
+Console.WriteLine(string.Join(',', result));
