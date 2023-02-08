adc7abd5fcd59b397f205286a5836d0dc5628dcd
Ilya <jyg3a125@gmail.com>
2023-02-08 20:37:01 +0500

    add Z1

---
+++ Z1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не корректное число. Повторите ввод");
+        }
+    }
+
+    return result;
+}
+int numberA = GetNumber("Введите первое число: ");
+int numberB = GetNumber("Введите второе число: ");
+
+double result = Math.Pow(numberA,numberB);
+Console.WriteLine(result);
